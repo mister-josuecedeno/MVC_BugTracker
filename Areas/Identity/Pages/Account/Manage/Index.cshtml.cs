93015b0ff60f9de427f7d2d0984f8f5644c08e33
mister-josuecedeno <josuecedeno@yahoo.com>
2021-06-22 19:45:52 -0500

    Add Fist Name, LastName, and User Profile Pic to Manage User Index

--- Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using MVC_BugTracker.Models;
+using MVC_BugTracker.Services.Interfaces;
 
 namespace MVC_BugTracker.Areas.Identity.Pages.Account.Manage
 {
@@ -14,16 +16,21 @@ namespace MVC_BugTracker.Areas.Identity.Pages.Account.Manage
     {
         private readonly UserManager<BTUser> _userManager;
         private readonly SignInManager<BTUser> _signInManager;
+        private readonly IBTFileService _fileService;
+
 
         public IndexModel(
             UserManager<BTUser> userManager,
-            SignInManager<BTUser> signInManager)
+            SignInManager<BTUser> signInManager, 
+            IBTFileService fileService)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _fileService = fileService;
         }
 
         public string Username { get; set; }
+        public string CurrentImage { get; set; }
 
         [TempData]
         public string StatusMessage { get; set; }
@@ -33,9 +40,20 @@ namespace MVC_BugTracker.Areas.Identity.Pages.Account.Manage
 
         public class InputModel
         {
+            [Display(Name = "First Name")]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 2)]
+            public string FirstName { get; set; }
+
+            [Display(Name = "Last Name")]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 2)]
+            public string LastName { get; set; }
+
             [Phone]
             [Display(Name = "Phone number")]
             public string PhoneNumber { get; set; }
+
+            [Display(Name = "Profile Image")]
+            public IFormFile NewImage { get; set; }
         }
 
         private async Task LoadAsync(BTUser user)
@@ -44,9 +62,12 @@ namespace MVC_BugTracker.Areas.Identity.Pages.Account.Manage
             var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
 
             Username = userName;
+            CurrentImage = _fileService.DecodeImage(user.AvatarFileData, user.AvatarContentType);
 
             Input = new InputModel
             {
+                FirstName = user.FirstName,
+                LastName = user.LastName,
                 PhoneNumber = phoneNumber
             };
         }
@@ -86,6 +107,37 @@ namespace MVC_BugTracker.Areas.Identity.Pages.Account.Manage
                     StatusMessage = "Unexpected error when trying to set phone number.";
                     return RedirectToPage();
                 }
+            }
+
+            bool hasChanged = false;
+
+            // Store First Name
+            if (user.FirstName != Input.FirstName)
+            {
+                // Store the new name
+                user.FirstName = Input.FirstName;
+                hasChanged = true;
+            }
+
+            // Store Last Name
+            if (user.LastName != Input.LastName)
+            {
+                // Store the new name
+                user.LastName = Input.LastName;
+                hasChanged = true;
+            }
+
+            // Store new image
+            if (Input.NewImage is not null)
+            {
+                user.AvatarFileData = await _fileService.EncodeFileAsync(Input.NewImage);
+                user.AvatarContentType = _fileService.ContentType(Input.NewImage);
+                hasChanged = true;
+            }
+
+            if (hasChanged)
+            {
+                await _userManager.UpdateAsync(user);
             }
 
             await _signInManager.RefreshSignInAsync(user);
