25f48c5d92eeba877581afa2f26512e1c98bb60d
mister-josuecedeno <josuecedeno@yahoo.com>
2021-07-16 11:18:54 -0500

    Make Project Id nullable

---
+++ Data/Migrations/20210716155614_002-Invite-Project-Id-Nullable.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MVC_BugTracker.Data.Migrations
+{
+    public partial class _002InviteProjectIdNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Invite_Project_ProjectId",
+                table: "Invite");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProjectId",
+                table: "Invite",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Invite_Project_ProjectId",
+                table: "Invite",
+                column: "ProjectId",
+                principalTable: "Project",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Invite_Project_ProjectId",
+                table: "Invite");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProjectId",
+                table: "Invite",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Invite_Project_ProjectId",
+                table: "Invite",
+                column: "ProjectId",
+                principalTable: "Project",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
